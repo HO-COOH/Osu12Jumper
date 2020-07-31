2d1b3c85a44dca8c5b35ac898594f021a72c52b9
Peter Li <szpeter@yeah.net>
2020-07-31 15:04:12 -0500

    Complete parser

--- OsuParser.hpp
+++ OsuParser.hpp
@@ -1,10 +1,10 @@
 /*
-    PLEASE READ:
+    PLEASE READ ME!
     
     This is a header-only library for parsing and generating an osu file.
-    The hit object is stored as:
+    The hit objects are stored as:
         std::vector<std::variant<...>>
-        
+    which makes more sense because you want to process the consecutive hit objects
     So if you want to loop through the objects and process them, you can use a switch statement
         switch(objects.index())
         {
@@ -13,11 +13,13 @@
             case 1:
                 ...
         }
-    Or better, use my provided object-specific iterator, process them by one type at a time:
+    Or use my provided object-specific iterator to process them by one type at a time:
         for(auto iter = file.begin<Type>(); iter != file.end<Type>(); ++iter)
         {...}
+     where Type can be <Circle>, <Slider> and <Spinner>
+    Or you want polymorphism and you want to treat them as a <HitObject> type, then you simply substitute <Type> -> <All>
     
-    I feel guilty of using runtime polymorphism, something like:
+    I feel guilty of storing each of them as a pointer, something like:
         std::vector<std::unique_ptr<HitObject>>
     So PLEASE DO NOT JUDGE!
 */
@@ -32,37 +34,70 @@
 #include <string>
 
 using namespace std::literals;
+
+/*Osu editor play field size*/
 constexpr int xMax = 512;
 constexpr int yMax = 384;
-constexpr unsigned ComboBit = 4;
+
+/*Some special bits representing hit object type
+     [type] is an 8-bit integer:
+         1 Circle
+         2 Slider
+         8 Spinner
+         *bit[2] set -> new combo 
+ */
+constexpr unsigned CirCleBit = 0b1;
+constexpr unsigned SliderBit = 0b10;
+constexpr unsigned ComboBit = 0b100;
+constexpr unsigned SpinnerBit = 0b1000;
 
 struct Coord
 {
     int x, y;
+    operator std::string() const
+    {
+        return std::to_string(x) + "," + std::to_string(y);
+    }
 };
+
+inline std::ostream& operator<<(std::ostream& os, Coord coord)
+{
+    os << '(' << coord.x << ", " << coord.y << ')';
+    return os;
+}
 
 /**
  * @brief Base class of all hit objects in osu
 */
 struct HitObject
 {
+    enum HitSound
+    {
+        Normal = 0,
+        Whistle = 1,
+        Finish = 2,
+        Clap = 3
+    };
     Coord coord;
     int time;
     bool combo;
-    HitObject(Coord coord, int time, bool combo=false):coord(coord), time(time), combo(combo){}
+    HitSound sound;
+
+    HitObject(Coord coord, int time, HitSound sound, bool combo=false):coord(coord), time(time), combo(combo), sound(sound){}
     virtual std::string toLine() = 0;
+    operator std::string() { return toLine(); }
     [[nodiscard]] float distanceTo(HitObject const& anotherObject) const
     {
-        return sqrt(pow(coord.x - anotherObject.coord.x, 2) + pow(coord.y - anotherObject.coord.y, 2));
+        return static_cast<float>(sqrt(pow(coord.x - anotherObject.coord.x, 2) + pow(coord.y - anotherObject.coord.y, 2)));
     }
     virtual ~HitObject() = default;
 };
 struct Circle final: HitObject
 {
-    Circle(Coord coord, int time, bool combo=false):HitObject{coord, time, combo} {}
+    Circle(Coord coord, int time, HitSound sound, bool combo=false):HitObject{coord, time, sound ,combo} {}
     std::string toLine() override
     {
-        return {};
+        return std::string(coord) + "," + std::to_string(time) +","+ std::to_string(combo ? 5 : 2) + ",";
     }
     ~Circle() override = default;
 };
@@ -70,14 +105,64 @@ struct Slider final: HitObject
 {
     enum CurveType
     {
-        Linear,
-        Circle
+        Bezier='B',
+        CatmullRom='C',
+        Linear='L',
+        Circle='C'
     };
+    CurveType curveType;
+    std::vector<Coord> curvePoint;
+    short slides;
+    double length;
 
-    Slider(Coord coord, int time, bool combo = false):HitObject{ coord, time, combo }{}
+    Slider(Coord coord, int time, CurveType curve, std::vector<Coord>&& curvePoint, short slides, double length,HitSound sound,bool combo = false)
+        :HitObject{ coord, time, sound ,combo },
+        curveType{ curve },
+        curvePoint{ std::move(curvePoint) },
+        slides{ slides },
+        length{length}
+    {}
     std::string toLine() override
     {
-        return {};
+        /*
+            Slider syntax:
+            x,y,time,type,hitSound,curveType|curvePoints,slides,length,edgeSounds,edgeSets,hitSample
+        */
+        std::string line;
+        line.reserve(200);
+        line += coord;
+        line += ',';
+        line += std::to_string(time) += ',';
+        line += std::to_string(combo ? 6 : 2) += ',';
+        line += std::to_string(static_cast<int>(sound)) += ',';
+        line += static_cast<char>(curveType);
+        for (auto const coord : curvePoint)
+        {
+            line += '|';
+            line += std::to_string(coord.x);
+            line += ':';
+            line += std::to_string(coord.y);
+        }
+        line += ',';
+        line += std::to_string(slides) += ',';
+        line += std::to_string(length) += ',';
+        return line;
+    }
+    static CurveType getCurveType(char curveTypeChar)
+    {
+        switch (curveTypeChar)
+        {
+        case 'B':
+            return CurveType::Bezier;
+        case 'C':
+            return CurveType::CatmullRom;
+        case 'L':
+            return CurveType::Linear;
+        case 'P':
+            return CurveType::Circle;
+        default:
+            return {};
+        }
     }
     ~Slider() override = default;
 };
@@ -85,10 +170,14 @@ struct Slider final: HitObject
 struct Spinner final:HitObject
 {
     int endTime;
-    Spinner(int time, int endTime):HitObject{{xMax/2,yMax/2}, time, true},endTime(endTime){}
+    Spinner(int time, int endTime, HitSound sound):HitObject{{xMax/2,yMax/2}, time, sound, true},endTime(endTime){}
+    /*
+        Spinner syntax:
+            x,y,time,type,hitSound,endTime,hitSample
+    */
     std::string toLine() override
     {
-        return {};
+        return std::string(coord) + "," + std::to_string(time) + "," + std::to_string(8) + "," + std::to_string(static_cast<int>(sound)) + "," + std::to_string(endTime) + ",";
     }
     ~Spinner() override = default;
 };
@@ -106,13 +195,14 @@ class OsuFile
     std::string title;
     std::string difficultyName;
 
-    void init(std::ifstream& file, bool lazy);
+    void init(std::ifstream& file, bool lazy, size_t count);
 public:
     using ObjectContainer = decltype(objects);
-    OsuFile(std::filesystem::directory_entry const& file, bool lazy=false);
-    OsuFile(std::ifstream&& file, bool lazy = false);
-    OsuFile const& operator>>(std::ofstream& out) const;
-    OsuFile const& operator>>(std::string_view fileName) const;
+    OsuFile(std::filesystem::directory_entry const& file, bool lazy=false, size_t objCount=SIZE_MAX);
+    OsuFile(std::ifstream&& file, bool lazy = false, size_t objCount=SIZE_MAX);
+    OsuFile(){}//TODO: implement
+    OsuFile const& operator>>(std::ofstream& out);
+    OsuFile const& operator>>(std::string_view fileName);
 
     template<typename ObjectType, typename = std::enable_if_t<std::is_base_of_v<HitObject, ObjectType>>>
     OsuFile& operator<<(ObjectType&& object);
@@ -127,6 +217,14 @@ public:
     [[nodiscard]]auto getBPM() const { return 60000.0 / interval; }
     [[nodiscard]]auto getTitle() const { return title; }
     [[nodiscard]]auto getDifficultyName() const { return difficultyName; }
+
+    template<typename ObjectType, typename = std::enable_if_t<std::is_base_of_v<HitObject, ObjectType> || std::is_same_v<ObjectType, All>>>
+    [[nodiscard]]auto getCount() const
+    {
+        return std::count_if(objects.cbegin(), objects.cend(), [](auto const& object) {return std::holds_alternative<ObjectType>(object); });
+    }
+
+
 
     friend std::ostream& operator<<(std::ostream& os, OsuFile const& osuFile)
     {
@@ -165,41 +263,89 @@ public:
     class ObjectIterator
     {
         ObjectContainer::iterator iter;
+        OsuFile const& ref;
     public:
-        ObjectIterator(ObjectContainer::iterator ref):iter(std::move(ref)){}
+        /*std::iterator<> is deprecated (in C++17) so we define these boilerplate ourselves*/
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = ObjectType;
+        using difference_type = typename ObjectContainer::iterator::difference_type;
+        using pointer = ObjectType*;
+        using reference = ObjectType&;
+
+        ObjectIterator(ObjectContainer::iterator iter, OsuFile const& ref):iter(std::move(iter)), ref(ref)
+        {
+            while (iter != ref.objects.end() && !std::holds_alternative<ObjectType>(*iter))
+                ++iter;
+        }
         ObjectType& operator*() const { return std::get<ObjectType>(*iter); }
         ObjectType* operator->() const { return &std::get<ObjectType>(*iter); }
         ObjectIterator& operator++()
         {
             do {
                 ++iter;
-            } while (!std::holds_alternative<ObjectType>(*iter));
+            } while (iter!=ref.objects.end() && !std::holds_alternative<ObjectType>(*iter));
             return *this;
         }
         ObjectIterator& operator--()
         {
             do {
                 --iter;
-            } while (!std::holds_alternative<ObjectType>(*iter));
+            } while (iter!=ref.objects.begin() && !std::holds_alternative<ObjectType>(*iter));
             return *this;
         }
         bool operator==(ObjectIterator const& rhs) const { return iter == rhs.iter; }
         bool operator!=(ObjectIterator const& rhs) const { return iter != rhs.iter; }
     };
 
+
     template<typename ObjectType, typename = std::enable_if_t<std::is_base_of_v<HitObject, ObjectType> || std::is_same_v<ObjectType, All>>>
     auto begin()
     {
-        return ObjectIterator<ObjectType>{objects.begin()};
+        return ObjectIterator<ObjectType>{objects.begin(), *this};
     }
     template<typename ObjectType, typename = std::enable_if_t<std::is_base_of_v<HitObject, ObjectType> || std::is_same_v<ObjectType, All>>>
     auto end()
     {
-        return ObjectIterator<ObjectType>{objects.end()};
+        return ObjectIterator<ObjectType>{objects.end(), *this};
     }
 };
 
-inline void OsuFile::init(std::ifstream& file, bool lazy)
+
+template<>
+class OsuFile::ObjectIterator<All>
+{
+    ObjectContainer::iterator iter;
+    OsuFile const& ref;
+public:
+    using iterator_category = std::bidirectional_iterator_tag;
+    using value_type = HitObject;
+    using difference_type = typename ObjectContainer::iterator::difference_type;
+    using pointer = HitObject*;
+    using reference = HitObject&;
+    ObjectIterator(ObjectContainer::iterator iter, OsuFile const& ref) :iter(std::move(iter)), ref(ref) {}
+    HitObject& operator*() const { return *std::visit([](auto&& obj) {return static_cast<HitObject*>(&obj); }, *iter); }
+    HitObject* operator->() const { return std::visit([](auto&& obj) {return static_cast<HitObject*>(&obj); }, *iter); }
+    ObjectIterator& operator++()
+    {
+        ++iter;
+        return *this;
+    }
+    ObjectIterator& operator--()
+    {
+        --iter;
+        return *this;
+    }
+    bool operator==(ObjectIterator const& rhs) const { return iter == rhs.iter; }
+    bool operator!=(ObjectIterator const& rhs) const { return iter != rhs.iter; }
+};
+
+template<>
+[[nodiscard]] inline auto OsuFile::getCount<All>() const
+{
+    return objects.size();
+}
+
+inline void OsuFile::init(std::ifstream& file, bool lazy, size_t count)
 {
     std::string line;
     line.reserve(100);
@@ -239,12 +385,12 @@ inline void OsuFile::init(std::ifstream& file, bool lazy)
     /*read hit objects*/
     if(!lazy)
     {
-        while(file)
+        while(!file.eof())
         {
             std::getline(file, line);
             if(line.find("HitObjects")!=std::string::npos)
             {
-                while(file)
+                while(std::getline(file, line) && count--)
                 {
                     /*
                         x,y,time,type,hitSound, [objectParams], [hitSample]
@@ -259,23 +405,68 @@ inline void OsuFile::init(std::ifstream& file, bool lazy)
                             2 Finish
                             3 Clap
                     */
-                    std::getline(file, line);
+                    
                     int x, y, t, type, sound;
                     sscanf(line.c_str(), "%d,%d,%d,%d,%d", &x, &y, &t, &type, &sound);
-                    if(type & 1)    //circles do not have additional [objectParams]
-                        objects.emplace_back(Circle{ {x,y}, t, static_cast<bool>(type & ComboBit) });
-                    else if (type & 2)
+                    if(type & CirCleBit)    //circles do not have additional [objectParams]
+                        objects.emplace_back(Circle{ 
+                            {x,y},
+                            t,
+                            static_cast<HitObject::HitSound>(sound),
+                            static_cast<bool>(type & ComboBit)
+                        });
+                    else if (type & SliderBit)
                     {
                         /*
-                            TODO: add slider parameters
+                            Slider syntax: x,y,time,type,hitSound,curveType|curvePoints,slides,length,edgeSounds,edgeSets,hitSample
+                                curveType(char):
+                                    B = bézier
+                                    C = centripetal catmull-rom
+                                    L = linear
+                                    P = perfect circle
+                                curvePoints(Pipe-separated list of strings):
+                                    Points used to construct the slider. Each point is in the format x:y.
+                                slides(integer):
+                                    Amount of times the player has to follow the slider's curve back-and-forth before the slider is complete.
+                                    It can also be interpreted as the repeat count plus one.
+                                length (Decimal):
+                                    Visual length in osu! pixels of the slider.
+                                
                         */
-                        objects.emplace_back(Slider{ {x,y}, t, static_cast<bool>(type & ComboBit) });
+                        char curveType{};
+                        sscanf(line.c_str(), "%d,%d,%d,%d,%d,%c", &x, &y, &t, &type, &sound, &curveType);
+                        /*read curve points*/
+                        std::vector<Coord> curvePoints;
+                        size_t const startIndex = line.find('|');
+                        size_t const endIndex = line.find(',', startIndex);
+                        size_t currentIndex = startIndex;
+                        while(currentIndex<=endIndex)
+                        {
+                            Coord coord;
+                            sscanf(line.c_str() + currentIndex+1, "%d:%d", &coord.x, &coord.y);
+                            curvePoints.push_back(coord);
+                            currentIndex = line.find('|', currentIndex+1);
+                        }
+                        short slides{};
+                        double length{};
+                        sscanf(line.c_str() + endIndex, "%d,%lf", &slides, &length);
+
+                        objects.emplace_back(Slider{ 
+                            {x,y},
+                            t,
+                            Slider::getCurveType(curveType),
+                            std::move(curvePoints),
+                            slides,
+                            length,
+                            static_cast<HitObject::HitSound>(sound),
+                            static_cast<bool>(type & ComboBit)
+                        });
                     }
-                    else if(type & 8)
+                    else if(type & SpinnerBit)
                     {
                         int endTime;
                         sscanf(line.c_str(), "%d,%d,%d,%d,%d,%d", &x, &y, &t, &type, &sound, &endTime);
-                        objects.emplace_back(Spinner{ t, endTime });
+                        objects.emplace_back(Spinner{ t, endTime, static_cast<HitObject::HitSound>(sound) });
                     }
                     length = t;
                 }
@@ -285,14 +476,14 @@ inline void OsuFile::init(std::ifstream& file, bool lazy)
     }
 }
 
-inline OsuFile::OsuFile(std::filesystem::directory_entry const& file, bool lazy)
+inline OsuFile::OsuFile(std::filesystem::directory_entry const& file, bool lazy, size_t objCount)
 {
     if (file.exists() && file.path().extension() == ".osu")
     {
         std::ifstream fs{ file };
         if(!fs.is_open())
             throw std::exception{ "File cannot be opened!" };
-        init(fs, lazy);
+        init(fs, lazy, objCount);
     }
     else
         throw std::exception{ ("File: "s + file.path().string() + " is not valid!").c_str() };
@@ -300,27 +491,34 @@ inline OsuFile::OsuFile(std::filesystem::directory_entry const& file, bool lazy)
 
 
 
-inline OsuFile::OsuFile(std::ifstream&& file, bool lazy)
+inline OsuFile::OsuFile(std::ifstream&& file, bool lazy, size_t objCount)
 {
     if (!file.is_open())
         throw std::exception{ "File cannot be opened!" };
-    init(file, lazy);
+    init(file, lazy, objCount);
 }
 
 
-inline OsuFile const& OsuFile::operator>>(std::ofstream& out) const
+inline OsuFile const& OsuFile::operator>>(std::ofstream& out)
 {
-    out << "osu file format v14\n";
-    //TODO: implement
+    out << "osu file format v14"
+        << "\n[Metadata]"
+        << "\nTitle:" << title
+        << "\nVersion:" << difficultyName
+        << "\n[Difficulty]"
+        << "\n[HitObjects]";
+    std::copy(begin<All>(), end<All>(), std::ostream_iterator<std::string>{out, "\n"});
+    //for (auto i = 0; i < objects.size(); ++i)
+    //    out << (*this)[i].toLine() << '\n';
     return *this;
 }
 
-inline OsuFile const& OsuFile::operator>>(std::string_view fileName) const
+inline OsuFile const& OsuFile::operator>>(std::string_view fileName)
 {
     std::ofstream fs{ fileName };
     if (!fs.is_open())
         throw std::exception{ ("Cannot open "s + fileName.data() + " for writing!").c_str() };
-    (*this) >> fs;
+    *this >> fs;
     return *this;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <random>
 #include "OsuParser.hpp"
+
+template<typename T>
+T PI = static_cast<T>(3.1415926535897L);
+
+/**
+ * @brief generate a random angle [0~360), then convert to rad
+ * @return a number indicating a random angle in rad
+*/
+static float randomAngle()
+{
+    static std::mt19937 eng{ std::random_device{}() };
+    static std::uniform_real_distribution<float> rdAngle{ 0,360 };
+    return rdAngle(eng) / (2 * PI<float>);
+}
+
+/**
+ * @brief Generate the position of next hit circle
+ * @param current the position of current circle
+ * @param distance the distance to the next hit circle
+ * @return the position of next hit circle
+*/
+static Coord nextCoord(Coord current, float distance)
+{
+    const auto angle = randomAngle();
+    return { static_cast<int>(current.x + cos(angle) * distance), static_cast<int>(current.y + sin(angle) * distance) };
+}
 
 int main()
 {
     OsuFile f{ std::ifstream{"1.osu"} };
-    for (auto i = 0; i < 10; ++i)
-        std::cout << f[i].time << '\n';
+    for(auto i=f.begin<Circle>(); i!=f.end<Circle>(); ++i)
+    {
+        std::cout << i->coord;
+        if (i->combo)
+            std::cout << "New combo!";
+        std::cout << '\n';
+    }
 }
